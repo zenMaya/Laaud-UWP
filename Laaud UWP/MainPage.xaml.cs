da75de8f8ba2da6587bd4753d5ea9a610f4d1a74
Jachym Tomasek <westerofwest@gmail.com>
2017-07-16 19:13:34 +0200

    Add part of assets, menu buttons

--- Laaud UWP/MainPage.xaml.cs
+++ Laaud UWP/MainPage.xaml.cs
@@ -13,18 +13,15 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace Laaud_UWP
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+
         public MainPage()
         {
             this.InitializeComponent();
+
         }
     }
 }
