7a9722695b09bc983de54371597dcb63f705a170
Čeněk Drbal <cendrb@gmail.com>
2017-08-12 20:42:10 +0200

    Add loggers

---
+++ Laaud UWP/Util/Logging/LaaudLogger.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laaud_UWP.Util.Log
+{
+    class LaaudLogger : ILogger
+    {
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            Debug.WriteLine(formatter(state, exception));
+        }
+    }
+}
---
+++ Laaud UWP/Util/Logging/LaaudLoggerProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laaud_UWP.Util.Log
+{
+    class LaaudLoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new LaaudLogger();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
